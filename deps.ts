142bba6cbd47742b111a1184f463ce881f70f5ca
Aruden <84431560+ArudenKun@users.noreply.github.com>
2022-07-02 23:22:36 +0800

    aasa

--- deps.ts
+++ deps.ts
@@ -6,5 +6,5 @@ export { windowsTerminalRouter } from "./routes/windows_terminal"
 
 import fs from "fs"
 
-const jsonData = fs.readFileSync("hashes.json", "utf-8")
+const jsonData = fs.readFileSync("./hashes.json", "utf-8")
 export const hashes = JSON.parse(jsonData)
