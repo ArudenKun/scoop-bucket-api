78141343df6f591a0e0167cc3006730ac6a3a9a5
Aruden <84431560+ArudenKun@users.noreply.github.com>
2022-07-02 23:22:40 +0800

    as

--- util/writeHash.ts
+++ util/writeHash.ts
@@ -1,7 +1,7 @@
 import fs from "fs"
 
 export async function writeHash(json: any) {
-    fs.writeFile("hashes.json", JSON.stringify(json), err => {
+    fs.writeFile("./hashes.json", JSON.stringify(json), err => {
         if (err) {
             console.log(err)
         }
