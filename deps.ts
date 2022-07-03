91406baee9287c1dd4bee72863de6e609d355225
Aruden <84431560+ArudenKun@users.noreply.github.com>
2022-07-03 08:09:00 +0800

    asdas

--- deps.ts
+++ deps.ts
@@ -5,6 +5,11 @@ export { steamlessRouter } from "./routes/steamless"
 export { windowsTerminalRouter } from "./routes/windows_terminal"
 
 import fs from "fs"
+import path from "path"
 
-const jsonData = fs.readFileSync("./hashes.json", "utf-8")
-export const hashes = JSON.parse(jsonData)
+// const jsonData = fs.readFileSync("./hashes.json", "utf-8")
+// export const hashes = JSON.parse(jsonData)
+
+const file = path.join(process.cwd(), '.', 'hashes.json');
+const json = fs.readFileSync(file, 'utf8');
+export const hashes = JSON.parse(json)
